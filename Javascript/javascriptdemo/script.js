1b60b6d14b845bb5c1b87d3eebe4369288d9dbce
bhasks2704 <126313867+bhasks2704@users.noreply.github.com>
2023-03-27 22:01:42 +0530

    pushing assignment notes and script cahnges

--- Javascript/javascriptdemo/script.js
+++ Javascript/javascriptdemo/script.js
@@ -1,6 +1,13 @@
-console.log("Hello world");
-console.error("Error Message");
+// console.log("Hello world");
+// console.error("Error Message");
 
-num=10.7;
+// num=10.7;
 
-console.log(typeof(num))
+// console.log(typeof(num))
+let sum = 0;
+for (let i = 1; i <= 50; i++) {
+  if (i % 2 == 0) {
+    sum += i;
+  }
+}
+console.log(sum);
